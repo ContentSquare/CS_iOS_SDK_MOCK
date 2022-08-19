3c1533d7b95c078cdf0817f68498db51b6c7b08f
Mac Mini 2018 <contentsquare.ci@gmail.com>
2022-08-19 11:29:36 +0200

    Bump version to 4.15.1

--- Package.swift
+++ Package.swift
@@ -32,6 +32,6 @@ let package = Package(
         .binaryTarget(
             name: "ContentsquareModule",
             url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.15.1/ContentsquareModuleSwiftPackage.xcframework.zip",
-            checksum: "8f80f7b1df5515fbdb71f7c23e11d10165757dd47da73b3c930ab0eec8b868ff"),
+            checksum: "67fe423ffd39a87858efcadf026824560b356e22373ebc4435d73f9bca82ce0e"),
     ]
 )
