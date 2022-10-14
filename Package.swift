7c03a73f0b9d8f174fb97e796ee879337648a987
WANG Jie <jacques09125715@gmail.com>
2022-10-14 17:27:09 +0200

    Update for carthage and SPM

--- Package.swift
+++ Package.swift
@@ -10,6 +10,10 @@ let package = Package(
         .library(
             name: "ContentsquareModule",
             targets: ["ContentsquareModuleWrapper"]),
+        .library(
+            name: "ContentsquareErrorAnalysisModule",
+            targets: ["ContentsquareErrorAnalysisModule"]
+        )
     ],
     dependencies: [
         .package(
@@ -33,5 +37,9 @@ let package = Package(
             name: "ContentsquareModule",
             url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.16.0/ContentsquareModuleSwiftPackage.xcframework.zip",
             checksum: "0c216ff0e0ad888ed7d3f64bdb24b8edfe4367487476804dad9ad498878877af"),
+        .binaryTarget(
+            name: "ContentsquareErrorAnalysisModule",
+            url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.16.0/ContentsquareErrorAnalysisModuleSwiftPackage.xcframework.zip",
+            checksum: "1344a580ff83955377dd8e224c26ee90e8e35191fb8cbaf3561c12165846d3b8"),
     ]
 )
