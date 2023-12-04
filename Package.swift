dbadf137893e5ce142ec6767cfc28dc4da315984
WANG Jie <jacques09125715@gmail.com>
2023-12-04 15:51:59 +0100

    Bump version to 4.27.1

--- Package.swift
+++ Package.swift
@@ -48,11 +48,11 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ContentsquareModule",
-            url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.27.0/ContentsquareModuleSwiftPackage.xcframework.zip",
+            url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.27.1/ContentsquareModuleSwiftPackage.xcframework.zip",
             checksum: "785505d76dd96d7a90e59c47d9df4d07db80b1ea31edf0acad1e20b148be0a51"),
         .binaryTarget(
             name: "ContentsquareErrorAnalysisModule",
-            url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.27.0/ContentsquareErrorAnalysisModuleSwiftPackage.xcframework.zip",
+            url: "https://github.com/ContentSquare/CS_iOS_SDK_MOCK/releases/download/4.27.1/ContentsquareErrorAnalysisModuleSwiftPackage.xcframework.zip",
             checksum: "b5b9f4847edd8a6fdb0716ede94f4d331f64f3986ec89f8bfb4988d5e6877f6a"),
     ]
 )
